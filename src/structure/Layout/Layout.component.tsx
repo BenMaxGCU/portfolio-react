062582d98734b5fb7e9ba9a14f5267623616f1d6
BenMaxGCU <bmaxwe200@caledonian.ac.uk>
2022-02-17 22:48:58 +0000

    Light mode styling

--- src/structure/Layout/Layout.component.tsx
+++ src/structure/Layout/Layout.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Main from '../Main/Main.component';
 import Nav from '../Nav/Nav.component';
 import './Layout.scss';
--- src/structure/Nav/Nav.component.tsx
+++ src/structure/Nav/Nav.component.tsx
@@ -1,20 +1,23 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useMantineColorScheme } from '@mantine/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ColourToggle from '../../components/utility/ColourToggle/ColourToggle.component';
 import './Nav.scss';
 
 function Nav() {
+  const { colorScheme } = useMantineColorScheme();
+
   return (
     <div className={'container'}>
-      <div className={'logo'}>
+      <div className={colorScheme === 'dark' ? 'logo' : 'logo--light'}>
         <h1>
           <Link to="/">Ben Maxwell</Link>
         </h1>
       </div>
 
       <nav className={'nav-menu-container'}>
-        <ul className={'nav-menu'}>
+        <ul className={colorScheme === 'dark' ? 'nav-menu' : 'nav-menu--light'}>
           <li>
             <a>
               <Link to="/case-studies">Case Studies</Link>
