6cef3d662a3cc8b61d2268dad2f1c8ea5676472c
BenMaxGCU <bmaxwe200@caledonian.ac.uk>
2022-02-17 22:23:17 +0000

    Fixed layout issues

--- src/components/main/Home/Home.component.tsx
+++ src/components/main/Home/Home.component.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { ArrowDownIcon } from '@modulz/radix-icons';
+import './Home.scss';
 
 function Home() {
   return (
     <div className={'hero'}>
-      <div className="hero-container">
+      <div className={'hero-container'}>
         <h1>Hello, I'm Ben.</h1>
         <h2>a software engineer.</h2>
-        <div className="icon-box">
-          <div className="icon">
-            <i className="fas fa-arrow-down"></i>
+        <div className={'icon-box'}>
+          <div className={'icon'}>
+            <ArrowDownIcon
+              style={{ width: 36, height: 36, color: '#f67280' }}
+            />
           </div>
         </div>
       </div>
