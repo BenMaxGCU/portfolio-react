07704b2f87a413454547087dafd799a89040da72
Bee Maxwell <ben24.maxwell@gmail.com>
2022-10-05 00:09:21 +0100

    feature: Added a POC mobile navigation, requires more testing

--- src/structure/Nav/Nav.component.tsx
+++ src/structure/Nav/Nav.component.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { useMantineColorScheme } from '@mantine/core';
+import { Burger, Drawer, useMantineColorScheme } from '@mantine/core';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ColourToggle from '../../components/utility/ColourToggle/ColourToggle.component';
 import styles from './Nav.module.scss';
@@ -9,24 +10,37 @@ type NavProps = {
 };
 
 function Nav({ visible }: NavProps) {
+  const [opened, setOpened] = useState(false);
+  const [windowDimension, setWindowDimension] = useState(null);
   const { colorScheme } = useMantineColorScheme();
 
-  return (
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimension(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const isMobile = windowDimension <= 1040;
+  const isDarkMode = colorScheme === 'dark';
+  const burgerTitle = opened ? 'Close navigation' : 'Open navigation';
+
+  return !isMobile ? (
     <div className={visible ? styles.container : styles['container--hidden']}>
-      <div
-        className={colorScheme === 'dark' ? styles.logo : styles['logo--light']}
-      >
+      <div className={isDarkMode ? styles.logo : styles['logo--light']}>
         <h1>
           <Link to="/">Ben Maxwell</Link>
         </h1>
       </div>
 
       <nav className={styles.navMenuContainer}>
-        <ul
-          className={
-            colorScheme === 'dark' ? styles.navMenu : styles['navMenu--light']
-          }
-        >
+        <ul className={isDarkMode ? styles.navMenu : styles['navMenu--light']}>
           <li>
             <Link to="/case-studies">Case Studies</Link>
           </li>
@@ -40,6 +54,60 @@ function Nav({ visible }: NavProps) {
           </li>
         </ul>
       </nav>
+    </div>
+  ) : (
+    <div
+      className={
+        visible ? styles['container--mobile'] : styles['container--hidden']
+      }
+    >
+      {!opened && (
+        <div className={isDarkMode ? styles.logo : styles['logo--light']}>
+          <h2>
+            <Link to="/">Ben Maxwell</Link>
+          </h2>
+        </div>
+      )}
+      <nav className={styles.navMenuContainer}>
+        <ul className={isDarkMode ? styles.navMenu : styles['navMenu--light']}>
+          <li>
+            <Burger
+              opened={opened}
+              onClick={() => setOpened((o) => !o)}
+              title={burgerTitle}
+            />
+          </li>
+          <li>
+            <a>
+              <ColourToggle />
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <Drawer
+        opened={opened}
+        onClose={() => setOpened(false)}
+        hideCloseButton={true}
+        padding="md"
+        size="sm"
+      >
+        <ul
+          className={isDarkMode ? styles.mobileNav : styles['mobileNav--light']}
+        >
+          <div className={isDarkMode ? styles.logo : styles['logo--light']}>
+            <h2>
+              <Link to="/">Ben Maxwell</Link>
+            </h2>
+          </div>
+          <li>
+            <Link to="/case-studies">Case Studies</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+        </ul>
+      </Drawer>
     </div>
   );
 }
